0021fa56294927419880201a6482b77d3531fa56
Silas Berger <silas.berger@outlook.com>
2024-05-29 15:47:42 +0200

    Improve LED toggle script.

--- demos/led_toggle.py
+++ demos/led_toggle.py
@@ -1,20 +1,25 @@
 # Connect LED Pin 8 + GND. Press the boot button to toggle the LED on and off.
 from machine import Pin
 
-led_on = False
-button_pressed = False
+led_on = True
+button_press_registered = False
 
 led = Pin(8, Pin.OUT)
 button = Pin(9, Pin.IN)
 
+def write_led():
+    if led_on:
+        led.on()
+    else:
+        led.off()
+
+write_led()
+
 while(True):
     if button.value() == 0:
-        if not button_pressed:
-            button_pressed = True
+        if not button_press_registered:
+            button_press_registered = True
             led_on = not led_on
-            if led_on:
-                led.on()
-            else:
-                led.off()
-    elif button_pressed:
-        button_pressed = False
+            write_led()
+    elif button_press_registered:
+        button_press_registered = False
